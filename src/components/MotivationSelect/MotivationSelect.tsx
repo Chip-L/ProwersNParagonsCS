e65538c5d8f5ecee907548224aa89063141f87a4
Chip Long <chip_long@yahoo.com>
2025-05-07 22:00:58 -0700

    added motivations

--- src/components/MotivationSelect/MotivationSelect.tsx
+++ src/components/MotivationSelect/MotivationSelect.tsx
@@ -1,80 +1,35 @@
-import { useState } from "react";
 import motivations from "@data/motivations.json" with { type: "MotivationType" };
-import {
-  Autocomplete,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
-} from "@mui/material";
+import { Autocomplete, TextField } from "@mui/material";
 
-interface MotivationType {
-  motivation: string;
-  description: string;
+const motivationsList = [...motivations];
+
+interface MotivationSelectProps {
+  selectedMotivation?: string;
+  onChange: (motivation: string) => void;
 }
 
-const otherMotivation: MotivationType = {
-  motivation: "Other",
-  description: "Create your own motivation.",
-};
-const motivationsList = [...motivations, otherMotivation];
+export const MotivationSelect = ({
+  selectedMotivation,
+  onChange,
+}: MotivationSelectProps) => {
+  const optionList = motivationsList.map((motivation) => motivation.motivation);
 
-// autocomplete code based on https://github.com/mui/material-ui/issues/43908
-export const MotivationSelect = () => {
-  const [selectedMotivation, setSelectedMotivation] =
-    useState<MotivationType>(otherMotivation);
+  const motivationDescription = motivationsList.find(
+    (motivation) => motivation.motivation === selectedMotivation,
+  )?.description;
 
-  const handleChange = (_event: unknown, newValue: MotivationType | null) => {
-    if (newValue === null) return;
-
-    setSelectedMotivation(newValue);
+  const handleChange = (_event: unknown, newValue: string | null) => {
+    onChange(newValue ?? "");
     console.log("Selected motivation: ", newValue);
   };
 
   return (
     <Autocomplete
       id="motivation-select"
-      options={motivationsList}
-      defaultValue={otherMotivation}
-      autoHighlight
-      getOptionLabel={(option) => option.motivation}
+      options={optionList}
+      title={motivationDescription}
       value={selectedMotivation}
       onChange={handleChange}
-      renderOption={(props, option) => {
-        const { key } = props;
-        return (
-          <TableRow
-            hover
-            key={key}
-          >
-            <TableCell>{option.motivation}</TableCell>
-            <TableCell>{option.description}</TableCell>
-          </TableRow>
-        );
-      }}
-      slotProps={{
-        listbox: {
-          component: (props) => (
-            <Paper style={{ maxHeight: 300, overflow: "auto" }}>
-              <Table
-                size="small"
-                stickyHeader
-              >
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Motivation</TableCell>
-                    <TableCell>Description</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody {...props}>{props?.children}</TableBody>
-              </Table>
-            </Paper>
-          ),
-        },
-      }}
       renderInput={(params) => (
         <TextField
           {...params}
